8826453a88de3d8c3688d957a6f16fd8e9d24d8a
Xiaohuimao <m0926xf@126.com>
2020-05-06 14:34:36 +0800

    弹窗api及分享知识点

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,78 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中ing',
+      duration: 3000,
+      icon: 'loading',
+      // image: ''
+      mask: true,
+      // 弹出后调用
+      success: function() {
+        console.log('展示弹窗成功')
+      },
+      fail: function() {
+        console.log('展示弹窗失败')
+      },
+      // 弹出后调用
+      complete: function() {
+        console.log('完成')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function(res) {
+        // 可以在res中知晓用户点击确认或取消
+        console.log(res)
+        if (res.cancel) {
+          console.log('用户点击了取消按钮')
+        }
+        if (res.confirm) {
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
+  handleShowLoading() {
+    // 网络请求一般使用该方法
+    wx.showLoading({
+      title: '加载ing',
+      mask: 'true'
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    setTimeout(()=> {
+      // 必须手动调用hideLoading才会让loading消失
+      wx.hideLoading()
+    },1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor: 'red',
+      success: function(res) {
+        console.log(res)
+        switch(res.tapIndex) {
+          case 0:
+            console.log('用户点击了相册')
+            break
+          case 1:
+            console.log('用户点击了拍照')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options) {
+    // 小程序不能直接分享到朋友圈，可以把二维码作为图片分享
+    return {
+      title: '你好呀，meng',
+      path: 'pages/about/about',
+      imageUrl: 'https://f11.baidu.com/it/u=1322252272,1472332239&fm=76'
+    }
   }
 })
